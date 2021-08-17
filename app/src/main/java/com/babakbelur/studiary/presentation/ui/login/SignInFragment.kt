c583a21113f86c37b4c4866e47d457678cfe31bc
RafliRasyidin <raflirasyidin20@gmail.com>
2021-08-17 23:27:33 +0700

    create sign in fragment

---
+++ app/src/main/java/com/babakbelur/studiary/presentation/ui/login/SignInFragment.kt
@@ -0,0 +1,15 @@
+package com.babakbelur.studiary.presentation.ui.login
+
+import android.os.Bundle
+import android.view.View
+import com.babakbelur.studiary.databinding.FragmentSignInBinding
+import com.babakbelur.studiary.presentation.base.BaseFragment
+
+class SignInFragment : BaseFragment<FragmentSignInBinding>(FragmentSignInBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
