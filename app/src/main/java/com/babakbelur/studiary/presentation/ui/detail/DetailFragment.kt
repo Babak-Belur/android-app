6aa5db4421a043d87b1b77bb63dab199b09cf652
RafliRasyidin <raflirasyidin20@gmail.com>
2021-08-17 23:24:15 +0700

    create detail fragment

---
+++ app/src/main/java/com/babakbelur/studiary/presentation/ui/detail/DetailFragment.kt
@@ -0,0 +1,18 @@
+package com.babakbelur.studiary.presentation.ui.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.babakbelur.studiary.R
+import com.babakbelur.studiary.databinding.FragmentDetailBinding
+import com.babakbelur.studiary.presentation.base.BaseFragment
+
+class DetailFragment : BaseFragment<FragmentDetailBinding>(FragmentDetailBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
