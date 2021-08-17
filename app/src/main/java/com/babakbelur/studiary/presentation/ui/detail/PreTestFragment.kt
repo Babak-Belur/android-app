a7a464a1eae280373b65645b2ebdbbb0d5135f20
RafliRasyidin <raflirasyidin20@gmail.com>
2021-08-17 23:27:48 +0700

    create pre test fragment

---
+++ app/src/main/java/com/babakbelur/studiary/presentation/ui/detail/PreTestFragment.kt
@@ -0,0 +1,8 @@
+package com.babakbelur.studiary.presentation.ui.detail
+
+import com.babakbelur.studiary.databinding.FragmentPreTestBinding
+import com.babakbelur.studiary.presentation.base.BaseFragment
+
+class PreTestFragment : BaseFragment<FragmentPreTestBinding>(FragmentPreTestBinding::inflate) {
+
+}
