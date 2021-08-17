164ed0f1cfc974dc1f2993996050a7c4e008083f
RafliRasyidin <raflirasyidin20@gmail.com>
2021-08-17 23:24:07 +0700

    create daily report fragment

---
+++ app/src/main/java/com/babakbelur/studiary/presentation/ui/detail/DailyReportFragment.kt
@@ -0,0 +1,9 @@
+package com.babakbelur.studiary.presentation.ui.detail
+
+import com.babakbelur.studiary.databinding.FragmentDailyReportBinding
+import com.babakbelur.studiary.presentation.base.BaseFragment
+
+class DailyReportFragment :
+    BaseFragment<FragmentDailyReportBinding>(FragmentDailyReportBinding::inflate) {
+
+}
