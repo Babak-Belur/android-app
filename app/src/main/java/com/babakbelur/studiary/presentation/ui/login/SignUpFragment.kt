7752f09c057073072e52ee1414af7fe9f77a6bb2
RafliRasyidin <raflirasyidin20@gmail.com>
2021-08-17 23:27:42 +0700

    create sign up fragment

---
+++ app/src/main/java/com/babakbelur/studiary/presentation/ui/login/SignUpFragment.kt
@@ -0,0 +1,8 @@
+package com.babakbelur.studiary.presentation.ui.login
+
+import com.babakbelur.studiary.databinding.FragmentSignUpBinding
+import com.babakbelur.studiary.presentation.base.BaseFragment
+
+class SignUpFragment : BaseFragment<FragmentSignUpBinding>(FragmentSignUpBinding::inflate) {
+
+}
