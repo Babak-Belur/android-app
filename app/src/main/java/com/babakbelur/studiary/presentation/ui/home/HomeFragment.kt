c4c92a645ecca2284ad660f35c9285f18f782e76
RafliRasyidin <raflirasyidin20@gmail.com>
2021-08-17 23:26:04 +0700

    create home fragment

---
+++ app/src/main/java/com/babakbelur/studiary/presentation/ui/home/HomeFragment.kt
@@ -0,0 +1,15 @@
+package com.babakbelur.studiary.presentation.ui.home
+
+import android.os.Bundle
+import android.view.View
+import com.babakbelur.studiary.databinding.FragmentHomeBinding
+import com.babakbelur.studiary.presentation.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
