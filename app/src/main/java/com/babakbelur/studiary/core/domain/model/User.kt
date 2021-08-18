e6d85c1d7368bf9d09071ffebb5d636b2e9dc0b6
RafliRasyidin <raflirasyidin20@gmail.com>
2021-08-18 17:10:20 +0700

    Create Data Class User

---
+++ app/src/main/java/com/babakbelur/studiary/core/domain/model/User.kt
@@ -0,0 +1,8 @@
+package com.babakbelur.studiary.core.domain.model
+
+data class User(
+    val idUser: Int = 0,
+    val username: String? = null,
+    val password: String? = null,
+    val name: String? = null,
+)
