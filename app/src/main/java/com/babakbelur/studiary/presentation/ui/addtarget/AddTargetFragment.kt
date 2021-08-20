fa0dd63394f22e63e786009927c5f4f2f4f9b6b7
RafliRasyidin <raflirasyidin20@gmail.com>
2021-08-21 00:49:30 +0700

    create add target fragment

---
+++ app/src/main/java/com/babakbelur/studiary/presentation/ui/addtarget/AddTargetFragment.kt
@@ -0,0 +1,19 @@
+package com.babakbelur.studiary.presentation.ui.addtarget
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.babakbelur.studiary.R
+import com.babakbelur.studiary.databinding.FragmentAddTargetBinding
+import com.babakbelur.studiary.presentation.base.BaseFragment
+
+class AddTargetFragment : BaseFragment<FragmentAddTargetBinding>(FragmentAddTargetBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
