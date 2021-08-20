282acd4f32118430fca56d130712bba5f46f385d
RafliRasyidin <raflirasyidin20@gmail.com>
2021-08-21 00:55:20 +0700

    setup horizontal button adapter

---
+++ app/src/main/java/com/babakbelur/studiary/presentation/adapter/HorizontalButtonAdapter.kt
@@ -0,0 +1,33 @@
+package com.babakbelur.studiary.presentation.adapter
+
+import androidx.recyclerview.widget.DiffUtil
+import com.babakbelur.studiary.core.domain.model.RangeTime
+import com.babakbelur.studiary.databinding.ItemHorizontalButtonBinding
+
+class HorizontalButtonAdapter : BaseAdapter<RangeTime, ItemHorizontalButtonBinding>(
+    ItemHorizontalButtonBinding::inflate,
+    diffCallback
+) {
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val rangeTime = getItem(position)
+        val binding = ItemHorizontalButtonBinding.bind(holder.itemView)
+        with(binding) {
+            tvStudyTime.text = rangeTime.rangeTime
+            root.setOnClickListener {
+                onItemClick?.invoke(rangeTime)
+            }
+        }
+    }
+
+    companion object {
+        private val diffCallback = object : DiffUtil.ItemCallback<RangeTime>() {
+            override fun areItemsTheSame(oldItem: RangeTime, newItem: RangeTime): Boolean {
+                return oldItem.rangeTime == newItem.rangeTime
+            }
+
+            override fun areContentsTheSame(oldItem: RangeTime, newItem: RangeTime): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
