1d720bfbadd1db51ff1b95bad4c8d5dfb126f45a
RafliRasyidin <raflirasyidin20@gmail.com>
2021-08-21 00:57:33 +0700

    setup navigation host fragment

--- app/src/main/java/com/babakbelur/studiary/presentation/ui/home/MainActivity.kt
+++ app/src/main/java/com/babakbelur/studiary/presentation/ui/home/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.babakbelur.studiary.presentation.ui.home
 
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.babakbelur.studiary.R
 import com.babakbelur.studiary.databinding.ActivityMainBinding
 import com.babakbelur.studiary.presentation.base.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
+    private lateinit var navHostController: NavController
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun getViewBinding(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostController = navHostFragment.navController
 
     }
 }
