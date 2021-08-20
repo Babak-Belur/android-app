55938b54baab924f3e62c41ccb8aca0b7a93fe22
RafliRasyidin <raflirasyidin20@gmail.com>
2021-08-21 00:59:50 +0700

    add function to navigate to main activity and sign ip fragment

--- app/src/main/java/com/babakbelur/studiary/presentation/ui/login/SignUpFragment.kt
+++ app/src/main/java/com/babakbelur/studiary/presentation/ui/login/SignUpFragment.kt
@@ -1,8 +1,46 @@
 package com.babakbelur.studiary.presentation.ui.login
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import com.babakbelur.studiary.R
 import com.babakbelur.studiary.databinding.FragmentSignUpBinding
 import com.babakbelur.studiary.presentation.base.BaseFragment
+import com.babakbelur.studiary.presentation.ui.home.MainActivity
 
 class SignUpFragment : BaseFragment<FragmentSignUpBinding>(FragmentSignUpBinding::inflate) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navigateToMainActivity()
+
+        navigateToSignInFragment()
+
+    }
+
+    private fun navigateToMainActivity() {
+        binding.btnSignUp.setOnClickListener {
+            val intent = Intent(activity, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun navigateToSignInFragment() {
+        binding.tvSignIn.setOnClickListener {
+            childFragmentManager.beginTransaction().apply {
+                replace(
+                    R.id.login_container,
+                    SignInFragment.newInstance(),
+                    SignInFragment::class.java.simpleName
+                )
+                commit()
+            }
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = SignUpFragment()
+    }
 }
