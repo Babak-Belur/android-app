06150dc9130f90d7df5fa037c76ed387d5e49dbe
RafliRasyidin <raflirasyidin20@gmail.com>
2021-08-21 22:21:56 +0700

    create add target view model

---
+++ app/src/main/java/com/babakbelur/studiary/presentation/ui/addtarget/AddTargetViewModel.kt
@@ -0,0 +1,9 @@
+package com.babakbelur.studiary.presentation.ui.addtarget
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AddTargetViewModel : ViewModel() {
+
+    val selectedDate: MutableLiveData<String> = MutableLiveData()
+}
