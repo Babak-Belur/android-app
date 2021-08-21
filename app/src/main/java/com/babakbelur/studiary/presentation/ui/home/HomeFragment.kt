93d07ad339cbab507164a4eb1bc115574583aa3c
RafliRasyidin <raflirasyidin20@gmail.com>
2021-08-21 22:23:37 +0700

    create function navigate to add new target

--- app/src/main/java/com/babakbelur/studiary/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/babakbelur/studiary/presentation/ui/home/HomeFragment.kt
@@ -2,6 +2,8 @@ package com.babakbelur.studiary.presentation.ui.home
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.babakbelur.studiary.R
 import com.babakbelur.studiary.databinding.FragmentHomeBinding
 import com.babakbelur.studiary.presentation.base.BaseFragment
 
@@ -10,6 +12,13 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        addNewTarget()
+    }
+
+    private fun addNewTarget() {
+        binding.fabAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_addTargetFragment)
+        }
     }
 
 }
