9fd6489435019f6fda6ba31409ae8362970ec543
RafliRasyidin <raflirasyidin20@gmail.com>
2021-08-23 22:51:19 +0700

    handle button clicked

--- app/src/main/java/com/babakbelur/studiary/presentation/adapter/StudyTimeAdapter.kt
+++ app/src/main/java/com/babakbelur/studiary/presentation/adapter/StudyTimeAdapter.kt
@@ -1,6 +1,8 @@
 package com.babakbelur.studiary.presentation.adapter
 
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
+import com.babakbelur.studiary.R
 import com.babakbelur.studiary.core.domain.model.RangeTime
 import com.babakbelur.studiary.databinding.ItemHorizontalButtonBinding
 
@@ -8,12 +10,31 @@ open class StudyTimeAdapter : BaseAdapter<RangeTime, ItemHorizontalButtonBinding
     ItemHorizontalButtonBinding::inflate,
     diffCallback
 ) {
+    private var indexButtonSelected = 0
+
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        indexButtonSelected = holder.adapterPosition
         val rangeTime = getItem(position)
         val binding = ItemHorizontalButtonBinding.bind(holder.itemView)
         with(binding) {
+
             tvStudyTime.text = rangeTime.rangeTime
             root.setOnClickListener {
+                if (indexButtonSelected == position) {
+                    btnStudyTime.setCardBackgroundColor(
+                        ContextCompat.getColor(
+                            holder.itemView.context,
+                            R.color.purple_500
+                        )
+                    )
+                } else {
+                    btnStudyTime.setCardBackgroundColor(
+                        ContextCompat.getColor(
+                            holder.itemView.context,
+                            android.R.color.transparent
+                        )
+                    )
+                }
                 onItemClick?.invoke(rangeTime)
             }
         }
@@ -32,4 +53,4 @@ open class StudyTimeAdapter : BaseAdapter<RangeTime, ItemHorizontalButtonBinding
     }
 }
 
-class FreeTimeAdapter: StudyTimeAdapter()
+class FreeTimeAdapter : StudyTimeAdapter()
