6ae03ae428dcffa57a69c982c2a3e59964e06b00
RafliRasyidin <raflirasyidin20@gmail.com>
2021-08-26 19:32:33 +0700

    add annotation experimental coroutines api

--- app/src/main/java/com/babakbelur/studiary/core/di/RepositoryModule.kt
+++ app/src/main/java/com/babakbelur/studiary/core/di/RepositoryModule.kt
@@ -1,13 +1,14 @@
 package com.babakbelur.studiary.core.di
 
-import com.babakbelur.studiary.core.data.AppRepository
-import com.babakbelur.studiary.core.data.remote.RemoteDataSource
+import com.babakbelur.studiary.core.domain.repository.AppRepository
+import com.babakbelur.studiary.core.data.remote.source.RemoteDataSource
 import com.babakbelur.studiary.core.data.remote.network.ApiService
 import com.babakbelur.studiary.core.domain.repository.IAppRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import javax.inject.Singleton
 
 @Module
@@ -19,6 +20,7 @@ object RepositoryModule {
     fun providesRemoteDataSource(apiService: ApiService): RemoteDataSource =
         RemoteDataSource(apiService)
 
+    @ExperimentalCoroutinesApi
     @Provides
     @Singleton
     fun providesAppRepository(remoteDataSource: RemoteDataSource): IAppRepository =
