bc5ecd8385d90b8616ca970502ba348803b62dac
RafliRasyidin <raflirasyidin20@gmail.com>
2021-08-26 19:34:31 +0700

    fix sign up request

--- app/src/main/java/com/babakbelur/studiary/presentation/ui/login/SignUpFragment.kt
+++ app/src/main/java/com/babakbelur/studiary/presentation/ui/login/SignUpFragment.kt
@@ -2,28 +2,93 @@ package com.babakbelur.studiary.presentation.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
 import com.babakbelur.studiary.R
+import com.babakbelur.studiary.core.data.ResultState
+import com.babakbelur.studiary.core.utils.onFailure
+import com.babakbelur.studiary.core.utils.onSuccess
 import com.babakbelur.studiary.databinding.FragmentSignUpBinding
 import com.babakbelur.studiary.presentation.base.BaseFragment
 import com.babakbelur.studiary.presentation.ui.home.MainActivity
+import com.babakbelur.studiary.presentation.ui.home.MainActivity.Companion.EXTRA_USER_ID
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SignUpFragment : BaseFragment<FragmentSignUpBinding>(FragmentSignUpBinding::inflate) {
+
+    private val viewModel: LoginViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        navigateToMainActivity()
-
         navigateToSignInFragment()
 
+        observeSignUpRequest()
     }
 
-    private fun navigateToMainActivity() {
+    private fun observeSignUpRequest() {
         binding.btnSignUp.setOnClickListener {
-            val intent = Intent(activity, MainActivity::class.java)
-            startActivity(intent)
+            val name = binding.etName.text.toString()
+            var username = binding.etUsernameSignUp.text.toString()
+            var password = binding.etPassword.text.toString()
+
+            viewModel.signUpRequest(name, username, password)
+            viewModel.signUp.observe(viewLifecycleOwner) { result ->
+                val isLoading = result is ResultState.Loading
+
+                binding.progressBarSignUp.isVisible = isLoading
+
+                result.onSuccess { resultData ->
+                    Toast.makeText(
+                        requireActivity(),
+                        getString(R.string.success_sign_up),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    username = resultData.data[0].username!!
+                    password = resultData.data[0].password!!
+                    observeSignInRequest(username, password)
+                }
+
+                result.onFailure { throwable ->
+                    Toast.makeText(
+                        requireActivity(),
+                        getString(R.string.error_sign_up),
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    Log.e(SignUpFragment::class.simpleName, throwable.message.toString())
+                }
+            }
         }
+    }
+
+    private fun observeSignInRequest(username: String, password: String) {
+        viewModel.signInRequest(username, password)
+        viewModel.signIn.observe(viewLifecycleOwner) { result ->
+
+            result.onSuccess { resultData ->
+                val userId = resultData.data.user[0].idUser
+                val name = resultData.data.user[0].name!!
+                navigateToMainActivity(userId, name)
+            }
+
+            result.onFailure { throwable ->
+                Log.e(SignUpFragment::class.simpleName, throwable.message.toString())
+            }
+        }
+    }
+
+    private fun navigateToMainActivity(userId: Int, name: String) {
+        Intent(requireActivity(), MainActivity::class.java).run {
+            putExtra(EXTRA_USER_ID, userId)
+            putExtra(MainActivity.EXTRA_NAME, name)
+            startActivity(this)
+        }
+
     }
 
     private fun navigateToSignInFragment() {
