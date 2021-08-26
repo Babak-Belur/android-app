d2a7c4a9559532c0cb04db4eaacc76eba49886f9
RafliRasyidin <raflirasyidin20@gmail.com>
2021-08-26 19:30:47 +0700

    refactor code

--- app/src/main/java/com/babakbelur/studiary/presentation/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/babakbelur/studiary/presentation/ui/login/LoginViewModel.kt
@@ -2,15 +2,24 @@ package com.babakbelur.studiary.presentation.ui.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.babakbelur.studiary.core.domain.model.UserItem
-import com.babakbelur.studiary.core.domain.usecase.IAppUseCase
+import com.babakbelur.studiary.core.domain.repository.IAppRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor(private val useCase: IAppUseCase) : ViewModel() {
+class LoginViewModel @Inject constructor(private val repository: IAppRepository) : ViewModel() {
 
-    fun signIn(userItem: UserItem) = useCase.signIn(userItem).asLiveData()
+    val signIn = repository.signIn.asLiveData(viewModelScope.coroutineContext)
+    val signUp = repository.signUp.asLiveData(viewModelScope.coroutineContext)
 
-    fun signUp(userItem: UserItem) = useCase.signUp(userItem).asLiveData()
+    fun signInRequest(username: String, password: String) = viewModelScope.launch {
+        repository.signInRequest(username, password)
+    }
+
+    fun signUpRequest(name: String, username: String, password: String) = viewModelScope.launch {
+        repository.signUpRequest(name, username, password)
+    }
 }
