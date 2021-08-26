7e0d13bc4ff8d85949fb5ad0fb07a0a25ac94a84
RafliRasyidin <raflirasyidin20@gmail.com>
2021-08-26 19:26:31 +0700

    remove unused contract

--- app/src/main/java/com/babakbelur/studiary/core/domain/usecase/AppInteractors.kt
+++ app/src/main/java/com/babakbelur/studiary/core/domain/usecase/AppInteractors.kt
@@ -1,59 +1,11 @@
 package com.babakbelur.studiary.core.domain.usecase
 
-import com.babakbelur.studiary.core.data.ResultState
-import com.babakbelur.studiary.core.data.remote.response.BaseDeleteResponse
-import com.babakbelur.studiary.core.domain.model.*
 import com.babakbelur.studiary.core.domain.repository.IAppRepository
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class AppInteractors @Inject constructor(private val appRepository: IAppRepository): IAppUseCase {
+class AppInteractors @Inject constructor(private val appRepository: IAppRepository) : IAppUseCase {
 
-    override fun signIn(user: UserItem): Flow<ResultState<User>> {
-        return appRepository.signIn(user)
-    }
-
-    override fun signUp(user: UserItem): Flow<ResultState<UserItem>> {
-        return appRepository.signUp(user)
-    }
-
-    override fun getTargetByUserId(idUser: Int): Flow<ResultState<List<TargetItem>>> {
-        return appRepository.getTargetByUserId(idUser)
-    }
-
-    override fun getTargetByTargetId(idTarget: Int): Flow<ResultState<List<TargetItem>>> {
-        return appRepository.getTargetByTargetId(idTarget)
-    }
-
-    override fun addTarget(target: TargetItem): Flow<ResultState<List<TargetItem>>> {
-        return appRepository.addTarget(target)
-    }
-
-    override fun deleteTarget(idUser: Int): Flow<ResultState<BaseDeleteResponse>> {
-        return appRepository.deleteTarget(idUser)
-    }
-
-    override fun getEvaluationById(idUser: Int): Flow<ResultState<EvaluationItem>> {
-        return appRepository.getEvaluationById(idUser)
-    }
-
-    override fun addEvaluation(evaluation: EvaluationItem): Flow<ResultState<List<EvaluationItem>>> {
-        return appRepository.addEvaluation(evaluation)
-    }
-
-    override fun deleteEvaluation(idEvaluation: Int): Flow<ResultState<BaseDeleteResponse>> {
-        return appRepository.deleteEvaluation(idEvaluation)
-    }
-
-    override fun getCourseById(idCourse: Int): Flow<ResultState<Course>> {
-        return appRepository.getCourseById(idCourse)
-    }
-
-    override fun addCourse(course: Course): Flow<ResultState<List<Course>>> {
-        return appRepository.addCourse(course)
-    }
-
-    override fun deleteCourse(idCourse: Int): Flow<ResultState<BaseDeleteResponse>> {
-        return appRepository.deleteCourse(idCourse)
+    override suspend fun signIn(username: String, password: String) {
+        return appRepository.signInRequest(username, password)
     }
 }
--- app/src/main/java/com/babakbelur/studiary/core/domain/usecase/IAppUseCase.kt
+++ app/src/main/java/com/babakbelur/studiary/core/domain/usecase/IAppUseCase.kt
@@ -1,33 +1,6 @@
 package com.babakbelur.studiary.core.domain.usecase
 
-import com.babakbelur.studiary.core.data.ResultState
-import com.babakbelur.studiary.core.data.remote.response.BaseDeleteResponse
-import com.babakbelur.studiary.core.domain.model.*
-import kotlinx.coroutines.flow.Flow
-
 interface IAppUseCase {
 
-    fun signIn(user: UserItem): Flow<ResultState<User>>
-
-    fun signUp(user: UserItem): Flow<ResultState<UserItem>>
-
-    fun getTargetByUserId(idUser: Int): Flow<ResultState<List<TargetItem>>>
-
-    fun getTargetByTargetId(idTarget: Int): Flow<ResultState<List<TargetItem>>>
-
-    fun addTarget(target: TargetItem): Flow<ResultState<List<TargetItem>>>
-
-    fun deleteTarget(idUser: Int): Flow<ResultState<BaseDeleteResponse>>
-
-    fun getEvaluationById(idUser: Int): Flow<ResultState<EvaluationItem>>
-
-    fun addEvaluation(evaluation: EvaluationItem): Flow<ResultState<List<EvaluationItem>>>
-
-    fun deleteEvaluation(idEvaluation: Int): Flow<ResultState<BaseDeleteResponse>>
-
-    fun getCourseById(idCourse: Int): Flow<ResultState<Course>>
-
-    fun addCourse(course: Course): Flow<ResultState<List<Course>>>
-
-    fun deleteCourse(idCourse: Int): Flow<ResultState<BaseDeleteResponse>>
+    suspend fun signIn(username: String, password: String)
 }
