f45660137e062d04b78dbbc968ec7595ee3447f4
RafliRasyidin <raflirasyidin20@gmail.com>
2021-08-26 19:30:27 +0700

    fix get all target

--- app/src/main/java/com/babakbelur/studiary/presentation/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/babakbelur/studiary/presentation/ui/home/HomeViewModel.kt
@@ -2,14 +2,19 @@ package com.babakbelur.studiary.presentation.ui.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.babakbelur.studiary.core.domain.repository.IAppRepository
 import com.babakbelur.studiary.core.domain.usecase.IAppUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(private val useCase: IAppUseCase) : ViewModel() {
+class HomeViewModel @Inject constructor(private val repository: IAppRepository) : ViewModel() {
 
-    fun getTargetByIdUser(idUser: Int) = useCase.getTargetByUserId(idUser).asLiveData()
+    val listTarget = repository.listTarget.asLiveData(viewModelScope.coroutineContext)
 
-    fun getTargetByIdTarget(idTarget: Int) = useCase.getTargetByTargetId(idTarget).asLiveData()
+    fun getAllTargetsUser(userId: Int) = viewModelScope.launch {
+        repository.getAllTargetsUser(userId)
+    }
 }
