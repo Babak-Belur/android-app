360feb5fe95149856616772af4e2c4703055bc3e
RafliRasyidin <raflirasyidin20@gmail.com>
2021-09-02 21:35:05 +0700

    handle add target

--- app/src/main/java/com/babakbelur/studiary/presentation/ui/addtarget/AddTargetFragment.kt
+++ app/src/main/java/com/babakbelur/studiary/presentation/ui/addtarget/AddTargetFragment.kt
@@ -4,23 +4,34 @@ import android.app.DatePickerDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.DatePicker
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.babakbelur.studiary.R
+import com.babakbelur.studiary.core.utils.onFailure
+import com.babakbelur.studiary.core.utils.onSuccess
 import com.babakbelur.studiary.databinding.FragmentAddTargetBinding
 import com.babakbelur.studiary.presentation.base.BaseFragment
+import com.babakbelur.studiary.presentation.ui.addtarget.PreTestFragment.Companion.ARG_COURSE_ID
+import com.babakbelur.studiary.presentation.ui.addtarget.PreTestFragment.Companion.ARG_TARGET_SCORE
+import com.babakbelur.studiary.presentation.ui.addtarget.PreTestFragment.Companion.ARG_TARGET_TIME
 import com.babakbelur.studiary.presentation.utils.ddMMMMyFormat
 import com.babakbelur.studiary.presentation.utils.ddMMMMyyyyFormat
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
 import javax.inject.Inject
+import kotlin.collections.ArrayList
 
 @AndroidEntryPoint
 class AddTargetFragment : BaseFragment<FragmentAddTargetBinding>(FragmentAddTargetBinding::inflate),
     DatePickerDialog.OnDateSetListener {
 
     private val viewModel: AddTargetViewModel by viewModels()
+
+    private val args: AddTargetFragmentArgs by navArgs()
 
     private var totalDays: Int = 0
     private var targetScore: Int = 0
@@ -40,6 +51,8 @@ class AddTargetFragment : BaseFragment<FragmentAddTargetBinding>(FragmentAddTarg
         setInitialDate()
 
         selectTargetScore()
+
+        observeAllCourses()
     }
 
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
@@ -50,16 +63,15 @@ class AddTargetFragment : BaseFragment<FragmentAddTargetBinding>(FragmentAddTarg
             append("-")
             append(dayOfMonth)
         }
-        selectedDay = stringBuilder.toString().ddMMMMyyyyFormat()
+        selectedDay = stringBuilder.toString()
         binding.tvDate.text = stringBuilder.toString().ddMMMMyFormat()
-        totalDays = viewModel.calculateTotalDays(currentDay, selectedDay)
     }
 
     private fun setInitialDate() {
         if (selectedDay.isEmpty()) {
             binding.tvDate.text = calendar.time.ddMMMMyFormat()
         } else {
-            binding.tvDate.text = selectedDay
+            binding.tvDate.text = selectedDay.ddMMMMyFormat()
         }
         currentDay = calendar.time.ddMMMMyyyyFormat()
     }
@@ -78,14 +90,73 @@ class AddTargetFragment : BaseFragment<FragmentAddTargetBinding>(FragmentAddTarg
         }
     }
 
+    private fun observeAllCourses() {
+        viewModel.getAllCourses()
+        viewModel.listCourses.observe(viewLifecycleOwner) { result ->
+
+            result.onSuccess { resultData ->
+                var idCourse: Int
+                val listCourses = resultData.data
+                val subjects = ArrayList<String>()
+                listCourses.forEach {
+                    idCourse = it.idCourse
+                    subjects.add("${it.courseName} - $idCourse")
+                }
+                val adapter = ArrayAdapter(requireContext(), R.layout.item_subject_course, subjects)
+                (binding.tfSubject.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+            }
+
+            result.onFailure { throwable ->
+                Log.e(AddTargetFragment::class.simpleName, throwable.message.toString())
+            }
+        }
+    }
+
     private fun addNewTarget() {
         binding.btnSubmit.setOnClickListener {
-            findNavController().navigate(R.id.action_addTargetFragment_to_preTestFragment)
+            val subject = binding.tfSubject.editText?.text.toString()
+            val isSubjectEmpty = subject.isEmpty()
+
+            val targetScore = binding.etTargetScore.text.toString()
+            val isScoreEmpty = targetScore.isEmpty() || targetScore == "0"
+
+            if (!isSubjectEmpty && !isScoreEmpty) {
+                val courseId = subject.replace("[^0-9]".toRegex(), "").toInt()
+                Bundle().run {
+                    putInt(ARG_USER_ID, args.userId)
+                    putString(ARG_TARGET_TIME, selectedDay)
+                    putInt(ARG_TARGET_SCORE, targetScore.toInt())
+                    putInt(ARG_COURSE_ID, courseId)
+                    findNavController().navigate(
+                        R.id.action_addTargetFragment_to_preTestFragment,
+                        this
+                    )
+                }
+            } else {
+                handleEmptyView(isSubjectEmpty, isScoreEmpty)
+            }
+
+        }
+    }
+
+    private fun handleEmptyView(isSubjectEmpty: Boolean, isScoreEmpty: Boolean) {
+        when {
+            isSubjectEmpty -> binding.tfSubject.error = getString(R.string.error_subject)
+            isScoreEmpty -> binding.etTargetScore.error = getString(R.string.error_target_score)
+            isSubjectEmpty && isScoreEmpty -> {
+                binding.etTargetScore.error = getString(R.string.error_target_score)
+                binding.tfSubject.error = getString(R.string.error_subject)
+            }
+            else -> {
+                binding.etTargetScore.error = null
+                binding.tfSubject.error = null
+            }
         }
     }
 
     companion object {
         private const val DATE_PICKER_TAG = "DatePicker"
+        const val ARG_USER_ID = "userId"
     }
 
 }
--- app/src/main/java/com/babakbelur/studiary/presentation/ui/addtarget/AddTargetViewModel.kt
+++ app/src/main/java/com/babakbelur/studiary/presentation/ui/addtarget/AddTargetViewModel.kt
@@ -3,16 +3,47 @@ package com.babakbelur.studiary.presentation.ui.addtarget
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.babakbelur.studiary.R
+import com.babakbelur.studiary.core.domain.usecase.IAppUseCase
 import com.babakbelur.studiary.presentation.adapter.TestQuestionAdapter
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.*
+import javax.inject.Inject
 import kotlin.math.abs
 
-class AddTargetViewModel : ViewModel() {
+@HiltViewModel
+class AddTargetViewModel @Inject constructor(private val useCase: IAppUseCase) : ViewModel() {
 
     private var _preTestScore: MutableLiveData<Int> = MutableLiveData()
     val preTestScore = _preTestScore
+
+    val addTarget = useCase.addTarget.asLiveData(viewModelScope.coroutineContext)
+
+    val addCourse = useCase.addCourse.asLiveData(viewModelScope.coroutineContext)
+
+    val listCourses = useCase.listCourses.asLiveData(viewModelScope.coroutineContext)
+
+    fun getAllCourses() = viewModelScope.launch {
+        useCase.getAllCourses()
+    }
+
+    fun addCourse(subject: String, description: String) = viewModelScope.launch {
+        useCase.addCourse(subject, description)
+    }
+
+    fun addTarget(
+        userId: Int,
+        courseId: Int,
+        preTestScore: Int,
+        targetScore: Int,
+        targetTime: String,
+    ) = viewModelScope.launch {
+        useCase.addTarget(userId, courseId, preTestScore, targetScore, targetTime)
+    }
 
     @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
     fun calculateTotalDays(currentDay: String, selectedDay: String): Int {
@@ -34,39 +65,39 @@ class AddTargetViewModel : ViewModel() {
                     score += 10
                     _preTestScore.value = score
                 }
-                1 -> if (checkedId == R.id.rb_answer_d){
+                1 -> if (checkedId == R.id.rb_answer_d) {
                     score += 10
                     _preTestScore.value = score
                 }
-                2 -> if (checkedId == R.id.rb_answer_b){
+                2 -> if (checkedId == R.id.rb_answer_b) {
                     score += 10
                     _preTestScore.value = score
                 }
-                3 -> if (checkedId == R.id.rb_answer_d){
+                3 -> if (checkedId == R.id.rb_answer_d) {
                     score += 10
                     _preTestScore.value = score
                 }
-                4 -> if (checkedId == R.id.rb_answer_c){
+                4 -> if (checkedId == R.id.rb_answer_c) {
                     score += 10
                     _preTestScore.value = score
                 }
-                5 -> if (checkedId == R.id.rb_answer_a){
+                5 -> if (checkedId == R.id.rb_answer_a) {
                     score += 10
                     _preTestScore.value = score
                 }
-                6 -> if (checkedId == R.id.rb_answer_b){
+                6 -> if (checkedId == R.id.rb_answer_b) {
                     score += 10
                     _preTestScore.value = score
                 }
-                7 -> if (checkedId == R.id.rb_answer_c){
+                7 -> if (checkedId == R.id.rb_answer_c) {
                     score += 10
                     _preTestScore.value = score
                 }
-                8 -> if (checkedId == R.id.rb_answer_c){
+                8 -> if (checkedId == R.id.rb_answer_c) {
                     score += 10
                     _preTestScore.value = score
                 }
-                9 -> if (checkedId == R.id.rb_answer_d){
+                9 -> if (checkedId == R.id.rb_answer_d) {
                     score += 10
                     _preTestScore.value = score
                 }
