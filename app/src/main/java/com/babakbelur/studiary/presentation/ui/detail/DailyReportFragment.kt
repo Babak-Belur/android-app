5c7502aec0bc6cda4810b6679e827dbc2db37841
RafliRasyidin <raflirasyidin20@gmail.com>
2021-09-02 21:31:47 +0700

    remove unused file

--- app/src/main/java/com/babakbelur/studiary/presentation/ui/detail/DailyReportFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.babakbelur.studiary.presentation.ui.detail
-
-import com.babakbelur.studiary.databinding.FragmentDailyReportBinding
-import com.babakbelur.studiary.presentation.base.BaseFragment
-
-class DailyReportFragment :
-    BaseFragment<FragmentDailyReportBinding>(FragmentDailyReportBinding::inflate) {
-
-}
