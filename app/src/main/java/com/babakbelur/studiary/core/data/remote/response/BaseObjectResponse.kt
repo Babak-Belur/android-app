1ba0894e105bb847e6243ef61ce407cc3504ba3d
RafliRasyidin <raflirasyidin20@gmail.com>
2021-09-02 21:35:24 +0700

    refactor file

--- app/src/main/java/com/babakbelur/studiary/core/data/remote/response/BaseObjectResponse.kt
+++ app/src/main/java/com/babakbelur/studiary/core/data/remote/response/BaseObjectResponse.kt
@@ -2,7 +2,11 @@ package com.babakbelur.studiary.core.data.remote.response
 
 import com.google.gson.annotations.SerializedName
 
-data class BaseResponse<T>(
+data class BaseObjectResponse<T>(
     @SerializedName("data")
     val `data`: T
+)
+
+data class BaseListResponse<T>(
+    val data: List<T>
 )
