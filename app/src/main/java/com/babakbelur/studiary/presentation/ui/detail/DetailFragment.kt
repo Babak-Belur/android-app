09257551b36f27c325ccedc1eea5be5537afc9a0
RafliRasyidin <raflirasyidin20@gmail.com>
2021-09-02 21:32:20 +0700

    handle detail fragment

--- app/src/main/java/com/babakbelur/studiary/presentation/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/babakbelur/studiary/presentation/ui/detail/DetailFragment.kt
@@ -1,18 +1,60 @@
 package com.babakbelur.studiary.presentation.ui.detail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.babakbelur.studiary.R
+import com.babakbelur.studiary.core.data.ResultState
+import com.babakbelur.studiary.core.utils.onFailure
+import com.babakbelur.studiary.core.utils.onSuccess
 import com.babakbelur.studiary.databinding.FragmentDetailBinding
 import com.babakbelur.studiary.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailFragment : BaseFragment<FragmentDetailBinding>(FragmentDetailBinding::inflate) {
+
+    private val viewModel: DetailViewModel by viewModels()
+
+    private val navArgs: DetailFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        observeDetailTarget()
+
+    }
+
+    private fun observeDetailTarget() {
+        viewModel.getDetailTarget(navArgs.targetId)
+        viewModel.detailTarget.observe(viewLifecycleOwner) { result ->
+            val isLoading = result is ResultState.Loading
+
+            binding.pbDetailTarget.isVisible = isLoading
+
+            result.onSuccess { resultData ->
+                val target = resultData.data[0]
+                binding.apply {
+                    tvSubject.text = target.course[0].courseName
+                    tvDescription.text = target.course[0].description
+                    tvDate.text = target.targetTime
+                    tvTargetScore.text = target.gradeTarget.toString()
+
+                }
+            }
+
+            result.onFailure { throwable ->
+                Toast.makeText(requireActivity(), getString(R.string.error), Toast.LENGTH_SHORT).show()
+                Log.e(DetailFragment::class.simpleName, throwable.message.toString())
+            }
+        }
+    }
+
+    companion object {
+        const val ARG_TARGET_ID = "targetId"
     }
 }
--- app/src/main/java/com/babakbelur/studiary/presentation/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/babakbelur/studiary/presentation/ui/detail/DetailViewModel.kt
@@ -2,10 +2,13 @@ package com.babakbelur.studiary.presentation.ui.detail
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.babakbelur.studiary.R
 import com.babakbelur.studiary.core.domain.usecase.IAppUseCase
 import com.babakbelur.studiary.presentation.adapter.TestQuestionAdapter
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,6 +16,17 @@ class DetailViewModel @Inject constructor(private val useCase: IAppUseCase) : Vi
 
     private var _evalScore: MutableLiveData<Int> = MutableLiveData()
     val evalScore = _evalScore
+
+    val detailTarget = useCase.detailTarget.asLiveData(viewModelScope.coroutineContext)
+    val predictedScore = useCase.predictedScore.asLiveData(viewModelScope.coroutineContext)
+
+    fun getPredictedScore(evaluationId: Int) = viewModelScope.launch {
+        useCase.getPredictedScore(evaluationId)
+    }
+
+    fun getDetailTarget(targetId: Int) = viewModelScope.launch {
+        useCase.getDetailTarget(targetId)
+    }
 
     fun calculatePreTestScore(testAdapter: TestQuestionAdapter) {
         var score = 0
